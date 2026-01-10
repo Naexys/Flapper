e08144feef03228a4e9faa11e66a4a1d12a637ab
Naexys <nicolasblaison@yahoo.fr>
2026-01-10 14:42:58 +0100

    Refactor main.lua with structured LOVE callbacks
    Added comments and organized LOVE2D callback functions for initialization, updating, drawing, and input handling. Improved code readability and structure by separating logic for top and bottom screens in love.draw.

--- main.lua
+++ main.lua
@@ -1,17 +1,33 @@
+-- File: main.lua
 
+-- Deactivate 3D mode
 love.graphics.set3D(false)
 
+-- Function to initialize the game
 function love.load()
     local caca = 0
 end
 
+-- Function to calculate each frame
+function love.update()
+    local caca = 0
+end
+
+-- Function to draw the calculated frame
 function love.draw(screen)
+    -- Draw on top screen
     if screen ~= "bottom" then
         local width, height = love.graphics.getDimensions(screen)
         love.graphics.print("Hello World!", width / 2, height / 2)
     end
+
+    -- Draw on bottom screen
+    if screen ~= "top" then
+        local caca = 0
+    end
 end
 
+-- Function to check for input
 function love.gamepadpressed(joystick, button)
     love.event.quit()
 end
