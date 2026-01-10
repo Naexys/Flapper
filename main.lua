fec2098f407514c87cb80e30cc9be4b78f6ced50
Naexys <nicolasblaison@yahoo.fr>
2026-01-10 17:03:13 +0100

    Add player movement and touch controls
    Initialized player properties and implemented vertical movement with speed affected by touch input. The player is now drawn as a rectangle on the top screen, and boundaries are enforced to keep the player within the visible area.

--- main.lua
+++ main.lua
@@ -4,17 +4,21 @@
 love.graphics.set3D(false)
 
 -- Initialize variables
-local score
-local playcount
-local highscore
-local player
+local score = 0
+local playcount = 0
+local highscore = 0
+local player = { x = 0, y = 0, speed = 0, maxSpeed = 5 }
+
+local width_top, height_top = love.graphics.getDimensions("top")
+local width_bottom, height_bottom = love.graphics.getDimensions("bottom")
 
 -- Array to keep track of touches data
 local touches = {}
 
 -- Deal with touch input
 function love.touchpressed(id, x, y, dx, dy, pressure)
-    touches[id] = {x = x, y = y}
+    touches[id] = { x = x, y = y }
+    player.speed = -3
 end
 
 -- Deal with touch release
@@ -24,25 +28,33 @@ end
 
 -- Function to initialize the game
 function love.load()
-    
+    player.x = width_top / 2
+    player.y = height_top / 2
 end
 
 -- Function to calculate each frame
 function love.update(dt)
-    
+    player.y = player.y + player.speed * dt
+    if player.y < 20 then
+        player.y = 20
+    elseif player.y > height_top - 20 then
+        player.y = height_top - 20
+    end
+    player.speed = player.speed + 1 * dt
+    if player.speed > player.maxSpeed then
+        player.speed = player.maxSpeed
+    end
 end
 
 -- Function to draw the calculated frame
 function love.draw(screen)
     -- Draw on top screen
     if screen ~= "bottom" then
-        local width, height = love.graphics.getDimensions(screen)
-        love.graphics.print("Hello World!", width / 2, height / 2)
+        love.graphics.rectangle("fill", player.x - 19, player.y - 19, 38, 38, 10, 10)
     end
 
     -- Draw on bottom screen
     if screen ~= "top" then
-        local width, height = love.graphics.getDimensions(screen)
         love.graphics.print("Press Start to quit.", 0, 0)
     end
 end
